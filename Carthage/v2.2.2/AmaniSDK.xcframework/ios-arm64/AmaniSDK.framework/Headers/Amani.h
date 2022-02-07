e2e9ec89c8cd34367ead443d0f1099db4c3b09c4
Münir Ketizmen <78314645+munir-amani@users.noreply.github.com>
2022-02-07 15:37:30 +0300

    v2.2.2
    xcode 12.5

---
+++ Carthage/v2.2.2/AmaniSDK.xcframework/ios-arm64/AmaniSDK.framework/Headers/Amani.h
@@ -0,0 +1,18 @@
+//
+//  Amani.h
+//  Amani
+//
+//  Created by Münir Ketizmen on 30.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Amani.
+FOUNDATION_EXPORT double AmaniVersionNumber;
+
+//! Project version string for Amani.
+FOUNDATION_EXPORT const unsigned char AmaniVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amani/PublicHeader.h>
+
+
